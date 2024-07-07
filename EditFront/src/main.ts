1a2f64432126f9f462c63413d975259f7bbdebb5
apolloYH <apollo>
2024-07-07 22:30:59 +0800

    hsihn

---
+++ EditFront/src/main.ts
@@ -0,0 +1,20 @@
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import './style.css'
+import App from './App.vue'
+import router from './router'
+
+const pinia=createPinia()
+const app = createApp(App)
+app.use(router)
+app.use(pinia)
+app.mount('#app')
